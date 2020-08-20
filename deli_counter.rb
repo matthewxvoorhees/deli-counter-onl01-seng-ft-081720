49c44e341b3ac3f67c63886e23995e74aca7df84
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-20 15:30:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,8 @@ def line (deli)
     puts "The line is currently empty."
   else
     line_string = "The line is currently:"
-    deli.each.with_index(1)
+    deli.each.with_index(1) do | person, i |
+      line_string << "#{i}. "
+      
   end
 end
